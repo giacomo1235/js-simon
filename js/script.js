21c997c05bad6cd7bc155fafe89a02a41971a277
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-04 17:25:18 +0100

    non riesco a confrontare i numeri random con quelli utente

--- js/script.js
+++ js/script.js
@@ -6,33 +6,50 @@
 
 var numeriRandom = [];
 var numeriUtente = [];
+var numeriSbagliati = [];
 var numeroUtente;
+var punteggio = 0;
+
 
 while (numeriRandom.length < 5) {
     var numeroCasuale = generaNumeriRandom(1, 9);
-    console.log(numeriRandom)
     numeriRandom.push(numeroCasuale);
 }
 document.getElementById('numeri-random').innerHTML = numeriRandom
 
-const randomNumberTimeOut = setTimeout(myRandomNumbers, 5000);
+const randomNumberTimeOut = setTimeout(myRandomNumbers, 2000);
 
 function myRandomNumbers() {
     document.getElementById('numeri-random').innerHTML = '';
-    promptNumeriUtente()
+    
 }
+
+const promptUtenteStart = setTimeout (promptNumeriUtente, 3000);
+
 function promptNumeriUtente(){
 while (numeriUtente.length < 5) {
     numeroUtente = parseInt(prompt("Inserisci un numero da 0 a 9"));
     numeriUtente.push(numeroUtente);
 }
-
+if (numeriRandom[1] == numeriUtente[1]) {
+    punteggio ++
+    
+} else {
+    numeriSbagliati.push(numeroUtente)
+}
+document.getElementById('numeri-random').innerHTML = numeriRandom
+document.getElementById('numeri-utente').innerHTML = 'numeri-utente' + numeriUtente
+document.getElementById('punteggio').innerHTML = 'punteggio' + punteggio
+document.getElementById('messaggio').innerHTML = 'num errati' + numeriSbagliati.length
 
 console.log(numeriUtente)
 }
   function generaNumeriRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+  
+
 
 
 
