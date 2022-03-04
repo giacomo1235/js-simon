3cdaa1c1a4980c9826d043a4b03809d067f237e8
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-04 16:03:04 +0100

    dopo 5 secondi esce il prompt ma i numeri non vanno via fin che non compilo

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
 // alla fine del timer userNumber chiede i 5 numeri 
 
 var numeriRandom = [];
+var numeriUtente = [];
+var numeroUtente;
 
 while (numeriRandom.length < 5) {
     var numeroCasuale = generaNumeriRandom(1, 9);
@@ -13,8 +15,28 @@ while (numeriRandom.length < 5) {
 }
 document.getElementById('numeri-random').innerHTML = numeriRandom
 
+const randomNumberTimeOut = setTimeout(myRandomNumbers, 5000);
+
+function myRandomNumbers() {
+    document.getElementById('numeri-random').innerHTML = '';
+    promptNumeriUtente()
+}
+function promptNumeriUtente(){
+while (numeriUtente.length < 5) {
+    numeroUtente = parseInt(prompt("Inserisci un numero da 0 a 9"));
+    numeriUtente.push(numeroUtente);
+}
+
+
+console.log(numeriUtente)
+}
   function generaNumeriRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
+
+
+
+
 
 
